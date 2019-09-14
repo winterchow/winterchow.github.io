8db35193b669b80baf4e84f59e9b2bbf213e8049
winterchow <yunqingchow@outlook.com>
2019-09-14 16:32:41 +0800

    add more algorithm

---
+++ Algorithm/Tree/SameTree.cs
@@ -0,0 +1,20 @@
+public class TreeNode {
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+    public TreeNode(int x) { val = x; }
+}
+
+public bool IsSameTree(TreeNode p, TreeNode q){
+    if(p == null && q == null){
+        return true;
+    }
+    if(p == null || q == null){
+        return false;
+    }
+    if(p.val == q.val){
+        return IsSameTree(p.left, q.left) && IsSameTree(p.right, q.right);
+    }
+
+    return false;
+}
--- Algorithm/Tree/UniqueBinarySearchTrees.cs
+++ Algorithm/Tree/UniqueBinarySearchTrees.cs
@@ -1,5 +1,12 @@
+// 卡特兰数
 public int NumTrees(int n) {
     int[] dp = new int[n + 1];
-
+    dp[0] = 1;
+    dp[1] = 1;
+    for(int i = 2; i <= n; i++){
+        for(int j = 1; j <= i; j++){
+            dp[i] += dp[i - j] * dp[j - 1];
+        }
+    }
     return dp[n];
 }
